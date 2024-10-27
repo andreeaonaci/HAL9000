8802048362b693a67a0102f8f3fb379804684d9f
andreeaonaci <andreeamariaonaci@gmail.com>
2024-10-27 20:06:15 +0200

    lab4 bun

--- HAL9000/src/sem.c
+++ HAL9000/src/sem.c
@@ -10,75 +10,56 @@ SemaphoreInit(
     IN      DWORD           InitialValue
 )
 {
+    ASSERT(NULL != Semaphore);
 
-	ASSERT(NULL != Semaphore);
+    memzero(Semaphore, sizeof(SEMAPHORE));
 
-	memzero(Semaphore, sizeof(SEMAPHORE));
+    LockInit(&Semaphore->SemLock);
 
-	LockInit(&Semaphore->SemLock);
+    Semaphore->Value = InitialValue;
+    Semaphore->InitValue = InitialValue;
 
-	Semaphore->Value = InitialValue;
-
-	InitializeListHead(&Semaphore->WaitingList);
-	InitializeListHead(&Semaphore->RunningList);
-
-	Semaphore->InitValue = InitialValue;
+    InitializeListHead(&Semaphore->WaitingList);
+    InitializeListHead(&Semaphore->RunningList);
 }
-
 
 void
 SemaphoreDown(
-	INOUT   PSEMAPHORE      Semaphore,
-	IN      DWORD           Value
-) 
-{
-	UNREFERENCED_PARAMETER(Value);
-	UNREFERENCED_PARAMETER(Semaphore);
-
-	PTHREAD pCurrentThread = GetCurrentThread();
-
-	ASSERT(NULL != Semaphore);
-	ASSERT(NULL != pCurrentThread);
-
-	INTR_STATE oldState;
-	INTR_STATE CpuState;
-
-	CpuState = CpuIntrDisable();
-
-	LockAcquire(&Semaphore->SemLock, &oldState);
-	
-	//verify if the current thread is already in the running list
-	//ASSERT(pCurrentThread->SemaphoreList is NOT in Sem->RunningList)
-
-	while (Semaphore->Value  >= Value)
-	{
-		InsertTailList(&Semaphore->WaitingList, &pCurrentThread->SemaphoreList);
-		ThreadTakeBlockLock();
-		LockRelease(&Semaphore->SemLock, oldState);
-		ThreadBlock();
-		//after thread is unblocked
-		LockAcquire(&Semaphore->SemLock, &oldState);
-	}
-
-	RemoveEntryList(&pCurrentThread->SemaphoreList);
-	InsertTailList(&Semaphore->RunningList, &pCurrentThread->SemaphoreList);
-	Semaphore->Value-=Value;
-
-
-	LockRelease(&Semaphore->SemLock, oldState);
-	CpuIntrSetState(CpuState);
-
-}
-void
-SemaphoreUp(
     INOUT   PSEMAPHORE      Semaphore,
     IN      DWORD           Value
-)
+) 
 {
-	UNREFERENCED_PARAMETER(Value);
-	UNREFERENCED_PARAMETER(Semaphore);
+    ASSERT(NULL != Semaphore);
+    ASSERT(Value > 0);
 
-	//verify if the current thread is already in the running 
-	//ASSERT(pCurrentThread->SemaphoreList is NOT in Sem)
-	// if sem->value + Value > sem->InitValue then sem->value = sem->InitValue
+    INTR_STATE oldState;
+    INTR_STATE cpuState;
+
+    cpuState = CpuIntrDisable();
+    PTHREAD pCurrentThread = GetCurrentThread();
+    ASSERT(NULL != pCurrentThread);
+
+    LockAcquire(&Semaphore->SemLock, &oldState);
+
+    // Verify if the current thread is not already in the running list
+    ASSERT(!IsListEntryInList(&Semaphore->RunningList, &pCurrentThread->SemaphoreList));
+
+    // Wait until there is sufficient value in the semaphore
+    while (Semaphore->Value < Value)
+    {
+        // Add to waiting list if there are not enough resources
+        InsertTailList(&Semaphore->WaitingList, &pCurrentThread->SemaphoreList);
+        ThreadTakeBlockLock();
+        LockRelease(&Semaphore->SemLock, oldState);
+        ThreadBlock();
+        LockAcquire(&Semaphore->SemLock, &oldState);
+    }
+
+    // Remove from waiting list and add to running list
+    RemoveEntryList(&pCurrentThread->SemaphoreList);
+    InsertTailList(&Semaphore->RunningList, &pCurrentThread->SemaphoreList);
+    Semaphore->Value -= Value;
+
+    LockRelease(&Semaphore->SemLock, oldState);
+    CpuIntrSetState(cpuState);
 }
